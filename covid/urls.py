a3c4f0ec6435161aced0d9abacf3a58a307cf40a
Devinlkw <72747840+Devinlkw@users.noreply.github.com>
2021-05-05 23:42:21 +0800

    Update urls.py

--- covid/urls.py
+++ covid/urls.py
@@ -4,9 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
     path('fetchLoginData', views.LoginAuthentication, name='fetch_login_data'),
+<<<<<<< Updated upstream
     path('covid', views.MainView.as_view(), name='main'),
     path('searchCase', views.SearchByCase, name='search_case'),
     path('searchDate', views.SearchByDate, name='search_date'),
+=======
+    path('main', views.Main, name='main'),
+    path('searchCase', views.SearchByCase, name='search_by_case'),
+    path('searchDate', views.SearchByDate, name='search_by_date'),
+>>>>>>> Stashed changes
 
     path('test', views.test, name='test'),
     path('caseForm', views.CaseFormView, name='case-form'),
